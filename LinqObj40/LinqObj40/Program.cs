fb8385254ba93abdeb7aca95e642756a39a13de7
Sirazetdinov Dinar 11-811 <43180262+Fiwer@users.noreply.github.com>
2019-05-14 18:38:00 +0300

    Add files via upload

---
+++ LinqObj40/LinqObj40/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace LINQObject40
+{
+    class Petrol
+    {
+        public string company { get; set; }
+        public string street { get; set; }
+        public int mark { get; set; }
+        public double money { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new List<Petrol>();
+
+            FileStream file = new FileStream(@"..\..\petrol.txt", FileMode.Open, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(file);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] attrClient = line.Split(' ');
+                data.Add(new Petrol
+                {
+                    company = attrClient[0],
+                    street = attrClient[1],
+                    mark = int.Parse(attrClient[2]),
+                    money = double.Parse(attrClient[3])
+                });
+            }
+
+            var query = data.OrderBy(x => x.street)
+                .GroupBy(x => x.street, (k, v) => new
+                {
+                    street = k,
+                    b92 = v.Where(y => y.mark == 92)
+                    .Select(y => y.mark).Count(),
+                    b95 = v.Where(y => y.mark == 95)
+                    .Select(y => y.mark).Count(),
+                    b98 = v.Where(y => y.mark == 98)
+                    .Select(y => y.mark).Count()
+                });
+                
+            
+                foreach (var item in query)
+                {
+                    Console.WriteLine("Улица:{0} 92:{1} 95:{2} 98:{3}", item.street, item.b92, item.b95, item.b98);
+                }
+            file.Close();
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
---
+++ LinqObj51/LinqObj51/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace LINQObject51
+{
+    class EGE
+    {
+        public int mat { get; set; }
+        public int rus { get; set; }
+        public int inf { get; set; }
+        public string surname { get; set; }
+        public string initials { get; set; }
+        public int school { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new List<EGE>();
+
+            FileStream file = new FileStream(@"..\..\EGE.txt", FileMode.Open, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(file);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] attrClient = line.Split(' ');
+                data.Add(new EGE
+                {
+                    mat = int.Parse(attrClient[0]),
+                    rus = int.Parse(attrClient[1]),
+                    inf = int.Parse(attrClient[2]),
+                    surname = attrClient[3],
+                    initials = attrClient[4],
+                    school = int.Parse(attrClient[5]),
+                });
+            }
+
+            var query = data.OrderBy(x => x.school)
+                .GroupBy(x => x.school, (k, v) => new
+                {
+                    school = k,
+                    inf = v.Select(y => y.inf).Max()
+                });
+
+            var a = 0;
+            foreach (var item in query)
+            {
+                if (item.school != a)
+                {
+                    Console.WriteLine("Номер школы:{0}, Балл ЕГЭ по информатике:{1} ", item.school, item.inf);
+                    a = item.school;
+                }
+            }
+            file.Close();
+            Console.ReadKey();
+
+        }
+    }
+}
