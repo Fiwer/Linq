065af7cfdcabcde7d46e40adb42b2800fe471ffd
Sirazetdinov Dinar 11-811 <43180262+Fiwer@users.noreply.github.com>
2019-05-14 18:39:15 +0300

    Add files via upload

---
+++ LinqObj 62/LINQ/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace LINQObject62
+{
+    class School
+    {
+        public int classroom { get; set; }
+        public string name { get; set; }
+        public int sum { get; set; }
+        public int alg { get; set; }
+        public int geo { get; set; }
+        public int inf { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var schools = new List<School>();
+
+            FileStream file = new FileStream(@"..\..\Lesson.txt", FileMode.Open, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(file);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] attrClient = line.Split(' ');
+                schools.Add(new School
+                {
+                    classroom = int.Parse(attrClient[0]),
+                    name = attrClient[1] + " " + attrClient[2],
+                    alg = int.Parse(attrClient[3]),
+                    geo = int.Parse(attrClient[4]),
+                    inf = int.Parse(attrClient[5]),
+                });
+            }
+ 
+            var nm = schools
+                .GroupBy(x => x.name)
+                .Select(i => new
+                {
+                    name = i
+                })
+                .OrderBy(e => e.name);
+
+            var group = schools
+                .Select(x => new { x.classroom, x.name, x.alg, x.geo, x.inf})
+                .OrderByDescending(classroom => classroom.classroom);
+
+            foreach (var item in group)
+            {
+                Console.WriteLine("Класс:{0},ФИО:{1}, Количество оценок: Алгебра:{2}, Геометрия:{3}, Информатика:{4} ", item.classroom, item.name, item.alg, item.geo, item.inf);
+            }
+
+            file.Close();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ LinqObj73/LINQ73/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace LINQObject73
+{
+    class Shop
+    {
+        public int year { get; set; }
+        public int codeP { get; set; }
+        public string street { get; set; }
+        public int codeS { get; set; }
+        public string shop { get; set; }
+        public int discount { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new List<Shop>();
+
+            FileStream file = new FileStream(@"..\..\shop.txt", FileMode.Open, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(file);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] attrClient = line.Split(' ');
+                data.Add(new Shop
+                {
+                    year = int.Parse(attrClient[0]),
+                    codeP = int.Parse(attrClient[1]),
+                    street = attrClient[2],
+                    codeS = int.Parse(attrClient[3]),
+                    shop = attrClient[4],
+                    discount = int.Parse(attrClient[5])
+                });
+            }
+
+
+            var query = data.Where(x => x.codeP == x.codeS)
+                .GroupBy(e => $"{e.shop} {e.street}", (k, v) => new
+                {
+                    shop = k,
+                    count = v.Count()
+                })
+                .OrderBy(x => x.shop);
+                
+                
+                       
+            foreach (var item in query)
+            {
+                    Console.WriteLine("{0} {1}", item.shop, item.count);
+
+            }
+            file.Close();
+
+            Console.ReadKey();
+
+        }
+    }
+}
