b2b4b46bdd46b47253bc270b81ccb972cdb493c5
Sirazetdinov Dinar 11-811 <43180262+Fiwer@users.noreply.github.com>
2019-05-14 18:36:34 +0300

    Add files via upload

---
+++ LinqObj29/LINQ29/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace LINQObject29
+{
+    class Debt
+    {
+        public int entrance { get; set; }
+        public int entranceNumber { get; set; }
+        public int number { get; set; }
+        public string surname { get; set; }
+        public double debt { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new List<Debt>();
+
+            FileStream file = new FileStream(@"..\..\debt.txt", FileMode.Open, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(file);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] attrClient = line.Split(' ');
+                data.Add(new Debt
+                {
+                    entrance = int.Parse(attrClient[0]),
+                    entranceNumber = int.Parse(attrClient[1]),
+                    number = int.Parse(attrClient[2]),
+                    surname = attrClient[3],
+                    debt = double.Parse(attrClient[4])
+                });
+            }
+
+            for (var i = 1; i < 5; i++)
+            {
+                double max = data.Where(x => x.entrance == i)
+                    .Max(a => a.debt);
+                var query = data.Where(x => x.debt == max)
+                    .GroupBy(e => $"Этаж:{e.entranceNumber}, Номер квартиры:{e.number}, Фамилия:{e.surname}, Долг:{e.debt}", (k, v) => new
+                    {
+                        maxDebt = k
+                    });
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine("{0}", item.maxDebt);
+
+                }
+            }
+            file.Close();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ LinqObj32/LINQ29/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace LINQObject32
+{
+    class Debt
+    {
+        public int entranceNumber { get; set; }
+        public int number { get; set; }
+        public string surname { get; set; }
+        public double debt { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new List<Debt>();
+
+            FileStream file = new FileStream(@"..\..\debt.txt", FileMode.Open, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(file);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] attrClient = line.Split(' ');
+                data.Add(new Debt
+                {
+                    entranceNumber = int.Parse(attrClient[0]),
+                    number = int.Parse(attrClient[1]),
+                    surname = attrClient[2],
+                    debt = double.Parse(attrClient[3])
+                });
+            }
+            for (var tu = 1; tu < 10; tu++)
+            {
+                double min = data.Where(e => e.entranceNumber == tu)
+                .Where(e => e.debt != 0)
+                    .Min(e => e.debt);
+                var query = data.Where(x => x.debt == min)
+                    .GroupBy(e => $"{e.entranceNumber} {e.debt}", (k, v) => new
+                    {
+                        minDebt = k
+                    });
+
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine("{0}", item.minDebt);
+                }
+            }
+            file.Close();
+
+            Console.ReadKey();
+
+        }
+    }
+}
